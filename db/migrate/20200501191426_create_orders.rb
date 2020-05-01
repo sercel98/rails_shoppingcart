516e25103015604b4c2ed99c45d5002e7afff219
Sergio <sergiocelemin1998@gmail.com>
2020-05-01 14:30:02 -0500

    add restrictions to migrations

--- db/migrate/20200501191426_create_orders.rb
+++ db/migrate/20200501191426_create_orders.rb
@@ -1,9 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.string :first_name
-      t.string :last_name
-      t.decimal :subtotal
+      t.string :first_name,:null => false 
+      t.string :last_name,:null => false
+      t.decimal :subtotal, :precision => 15, :scale => 3
 
       t.timestamps
     end
