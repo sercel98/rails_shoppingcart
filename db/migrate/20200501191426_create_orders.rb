6ad930b2303095401653227e24845c9aec847f53
Sergio <sergiocelemin1998@gmail.com>
2020-05-01 16:16:31 -0500

    install webpack and start categoryview

--- db/migrate/20200501191426_create_orders.rb
+++ db/migrate/20200501191426_create_orders.rb
@@ -3,7 +3,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
     create_table :orders do |t|
       t.string :first_name,:null => false 
       t.string :last_name,:null => false
-      t.decimal :subtotal, :precision => 15, :scale => 3
+      t.decimal :subtotal,:null => false, :precision => 15, :scale => 3
 
       t.timestamps
     end
